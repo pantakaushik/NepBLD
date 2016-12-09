647d803342cee1ebdb0f0dc7cc4e57068280e033
Panta kaushik <mailtokaushikpanta@gmail.com>
2016-12-09 22:09:47 +0545

    urls for registering and success are created

--- NepBLD/NepBLD/Index/urls.py
+++ NepBLD/NepBLD/Index/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
-from Index.views import IndexView
+from Index.views import IndexView,RegisterView,SuccessView
 
 urlpatterns = [
-	url(r'^$', IndexView.as_view()),
+	url(r'^$', IndexView.as_view(), name='index'),
+	url(r'^register/$', RegisterView.as_view(), name='register'),
+	url(r'^success/$', SuccessView.as_view(), name='success'),
 ]
