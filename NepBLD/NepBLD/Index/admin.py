972c408c06a6fb636dc08b198635eb1476cd229e
kauseekpanta014 <kauseekpanta014@gmail.com>
2016-12-09 17:18:31 +0545

    App registered in admin

--- NepBLD/NepBLD/Index/admin.py
+++ NepBLD/NepBLD/Index/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from Index.models import RegisterClass
 # Register your models here.
+class RegisterClassAdmin(admin.ModelAdmin):
+	list_display = ('first_name','middle_name','last_name','name_slug')
+
+admin.site.register(RegisterClass, RegisterClassAdmin)
