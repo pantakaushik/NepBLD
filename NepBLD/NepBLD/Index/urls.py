19812b7f60e4a6d3b2b088df86c7495a9cf30d0d
Panta kaushik <mailtokaushikpanta@gmail.com>
2016-12-09 23:29:13 +0545

    url for search form is added

--- NepBLD/NepBLD/Index/urls.py
+++ NepBLD/NepBLD/Index/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
-from Index.views import IndexView,RegisterView,SuccessView
+from Index.views import IndexView,RegisterView,SuccessView, SearchView
 
 urlpatterns = [
 	url(r'^$', IndexView.as_view(), name='index'),
 	url(r'^register/$', RegisterView.as_view(), name='register'),
 	url(r'^success/$', SuccessView.as_view(), name='success'),
+	url(r'^searchdata/$', SearchView.as_view(), name='search'),
+
+
 ]
