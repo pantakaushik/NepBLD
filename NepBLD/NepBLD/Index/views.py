65ee30506ac6bd4a630f7ca23aaea9d8863a1419
Panta kaushik <mailtokaushikpanta@gmail.com>
2016-12-09 22:11:36 +0545

    views for registering new donor is created

--- NepBLD/NepBLD/Index/views.py
+++ NepBLD/NepBLD/Index/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
+from Index.forms import RegisterClassForm
 # Create your views here.
 
 
@@ -8,3 +9,24 @@ class IndexView(View):
 
 	def get(self,request):
 		return render(request, self.template_name )
+
+class RegisterView(View):
+	template_name = 'register.html'
+	form_class = RegisterClassForm
+
+	def get(self, request, *args,**kwargs):
+		form = self.form_class()
+		return render(request, self.template_name,{'form':form})
+		
+
+	def post(self, request, *args,**kwargs):
+		form = self.form_class(request.POST)
+		if form.is_valid():
+			form.save(commit=True)
+			return redirect('/success/')
+		return render(request,self.template_name,{'form':form})
+class SuccessView(View):
+	template_name='success.html'
+
+	def get(self, request):
+		return render(request, self.template_name)
