9aabb2ca54616adccd174c2efcfeaef93ac93e9f
kauseekpanta014 <kauseekpanta014@gmail.com>
2016-12-09 16:46:56 +0545

    Settings file and Urls file are modified

--- NepBLD/NepBLD/NepBLD/urls.py
+++ NepBLD/NepBLD/NepBLD/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('Index.urls')),
+    url(r'^', include('Index.urls')),		#url for Index App is created
 ]
