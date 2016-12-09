dce85e692e5b528e70911aca02b23229eabc27a6
Panta kaushik <mailtokaushikpanta@gmail.com>
2016-12-09 22:11:03 +0545

    added modeladmin for models

--- NepBLD/NepBLD/Index/admin.py
+++ NepBLD/NepBLD/Index/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from Index.models import RegisterClass
 # Register your models here.
 class RegisterClassAdmin(admin.ModelAdmin):
-	list_display = ('first_name','middle_name','last_name','name_slug')
+	list_display = ('first_name','middle_name','last_name','gender','bloodgroup','city','address','email','contact_number')
 
 admin.site.register(RegisterClass, RegisterClassAdmin)
