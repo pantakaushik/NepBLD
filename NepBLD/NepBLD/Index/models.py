b05fbc669d868eeaf492881e81064a33d77bbabb
kauseekpanta014 <kauseekpanta014@gmail.com>
2016-12-09 17:19:00 +0545

    Models are created

--- NepBLD/NepBLD/Index/models.py
+++ NepBLD/NepBLD/Index/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from autoslug import AutoSlugField
 
-# Create your models here.
+
+
+class RegisterClass(models.Model):
+	first_name = models.CharField(max_length=100, verbose_name='First Name')
+	middle_name = models.CharField(max_length=100, verbose_name='Middle Name')
+	last_name = models.CharField(max_length=100,  verbose_name='last Name')
+	name_slug = AutoSlugField(populate_from='first_name', unique_with=['first_name','last_name'])
+
+	def __str__(self):
+		return self.first_name
