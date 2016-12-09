40dfa663d2d7b47d4701a0990f121d4d5d830435
Panta kaushik <mailtokaushikpanta@gmail.com>
2016-12-09 23:30:07 +0545

    view moudule for searching is added to views.py

--- NepBLD/NepBLD/Index/views.py
+++ NepBLD/NepBLD/Index/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
-from Index.forms import RegisterClassForm
+from Index.forms import RegisterClassForm,SearchForm
+from Index.models import RegisterClass
 # Create your views here.
 
 
 class IndexView(View):
 	template_name = 'index.html'
+	form_class = SearchForm
 
 	def get(self,request):
-		return render(request, self.template_name )
+		form = self.form_class()
+		# bloodgroup = self.request.GET.get('bloodgroup')
+		# city = self.request.GET.get('city')
+		# result = RegisterClass.objects.filter(bloodgroup=bloodgroup,city=city)
+		return render(request, self.template_name,{'form':form})
 
 class RegisterView(View):
 	template_name = 'register.html'
@@ -25,8 +31,21 @@ class RegisterView(View):
 			form.save(commit=True)
 			return redirect('/success/')
 		return render(request,self.template_name,{'form':form})
+
 class SuccessView(View):
 	template_name='success.html'
 
 	def get(self, request):
 		return render(request, self.template_name)
+
+class SearchView(View):
+	template_name = 'search.html'
+	form_class = SearchForm
+
+	def get(self, request):
+		form = self.form_class()
+		bloodgroup = self.request.GET.get('bloodgroup')
+		city = self.request.GET.get('city')
+		result = RegisterClass.objects.filter(bloodgroup=bloodgroup,city=city)
+		return render(request, self.template_name,{'form':form,'result':result,'bloodgroup':bloodgroup,'city':city})
+
